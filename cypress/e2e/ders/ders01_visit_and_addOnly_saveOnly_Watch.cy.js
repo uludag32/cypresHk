6ee741531d3b133c1c5f329fa4324b33a47fcab3
uludag32 <ouludag0304@gmail.com>
2025-02-13 09:48:56 +0100

    first commit

---
+++ cypress/e2e/ders/ders01_visit_and_addOnly_saveOnly_Watch.cy.js
@@ -0,0 +1,22 @@
+describe('cy.visit addOnly saveOnly watchForFileChanges', () => {
+    it('cy.visit full', () => {
+        cy.visit(`https://demoqa.com`)
+    });
+
+    it('cy.visit url', () => {
+        cy.visit({
+            url:`https://demoqa.com`})
+            
+            });
+
+    it('cy.visit shortcut', () => {
+        cy.visit(`/`)
+    });
+    it.only('cy.visit shortcut plus', () => {
+        cy.visit(`/elements`)
+        cy.wait(1000)
+    });
+
+
+
+});
